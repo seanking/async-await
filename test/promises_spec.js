0c1bf4c35754708ffe4b57b6c4df5606aed94e64
Sean King <r.sean.king@gmail.com>
2016-03-04 13:46:31 -0500

    Rename file

---
+++ test/promises_spec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Promise = require('promise');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+describe('Async-Await ', () => {
+  it('should use promise', () => {
+    // Given
+    const generateGreeting = (name) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(`Hello ${name}!`);
+        }, 1500);
+      });
+    };
+
+    // When
+    const promise = generateGreeting('Sean');
+
+    // Then
+    return expect(promise).to.eventually.equal('Hello Sean!');
+  });
+
+  it('should use promise.resolve', () => {
+    // Given
+    const message = 'Say Hello!';
+
+    // When
+    const promise = Promise.resolve(message);
+
+    // Then
+    return expect(promise).to.eventually.equal(message);
+  });
+
+  it('should use promise.reject', () => {
+    // Given
+    const errorMessage = 'Help! Error!';
+
+    // When
+    const promise = Promise.reject(new Error(errorMessage));
+
+    // Then
+    return expect(promise).to.eventually.rejectedWith(errorMessage);
+  });
+});
